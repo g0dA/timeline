275dbf2fcdefb1ccc904c31cb172d2575f4cde4a
L_AnG <544972149@qq.com>
2017-08-15 19:01:37 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+                createStoryJS({
+                    type:       'timeline',
+                    width:      '100%',
+                    height:     '100%',
+                    source:     'https://docs.google.com/spreadsheets/d/13g_3BlIYOUKT-2EfsEKgtZqK8A_3XIT1kZN0B54U9WQ/pubhtml?gid=0&single=true',
+                    embed_id:   'my-timeline'
+                });
+            });
